96c99abe0b56ba467c6e5e338128a088c45463dd
JiahaoYang <374084857@qq.com>
2018-03-11 16:42:09 +0800

    购物车与订单支付

--- src/main/java/com/yjh/TmallApplication.java
+++ src/main/java/com/yjh/TmallApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@MapperScan("com.yjh.mapper")
-@ComponentScan(basePackages = "com.yjh")
+@MapperScan("com.yjh.mapper")   //mybatis配置文件映射位置
+@EnableTransactionManagement    //开启事务管理
 public class TmallApplication {
 
     public static void main(String[] args) {
